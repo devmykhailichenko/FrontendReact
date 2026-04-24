89b91760f2e941102a8631ce8d44f91d557af244
devmykhailichenko <ihor.mykhailichenko@gmail.com>
2026-04-24 21:30:43 +0300

    Class Components start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React  from "react";
 import Message from "./components/Message/Message.jsx";
 import Wrapper from "./components/Wrapper/Wrapper.jsx";
+import Counter from "./components/Counter/Counter.jsx";
+import TaskList from "./components/TaskList/TaskList.jsx";
 
 class App extends React.Component {
   render() {
     return (
         <div>
+          <TaskList />
+          <Counter start={5} />
+          <Counter start={2} />
+          <Counter start={1} />
           <Message />
           <Wrapper />
         </div>
